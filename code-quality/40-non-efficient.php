8b1d6ef277e9aeddb60c5e6b8a0ba354f17a78b4
Camilo Sperberg <me@unreal4u.com>
2015-02-18 22:10:33 +0100

    Micro-operations added

--- code-quality/40-non-efficient.php
+++ code-quality/40-non-efficient.php
@@ -4,6 +4,8 @@ if (is_writable('test.txt')) {
     unlink('test.txt');
 }
 
-for ($i = 0; $i < 100; $i++) {
-    file_put_contents('test.txt', sprintf('Current value: %d'.PHP_EOL, $i), FILE_APPEND);
+for ($i = 1; $i <= 100; $i++) {
+    if (isset($i) && !empty($i) && is_numeric($i) && is_int($i)) {
+        file_put_contents('test.txt', 'Current value: '.$i.PHP_EOL, FILE_APPEND);
+    }
 }
